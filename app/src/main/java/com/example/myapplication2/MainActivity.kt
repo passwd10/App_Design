bd54e617a2079fc708be5a1d1f9a93ba55220132
passwd10 <parkinseo94@naver.com>
2019-09-08 23:42:30 +0900

    Add Activity

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -2,12 +2,36 @@ package com.example.myapplication2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.toss_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setContentView(R.layout.toss_timeline)
+        setContentView(R.layout.toss_main)
+
+        var cnt = 1
+        var result_num = findViewById(R.id.tv_result_window)
+        val btn1:Button = findViewById(R.id.btn1)
+        val btn2:Button = findViewById(R.id.btn2)
+
+
+        btn1.setOnClickListener(View.OnClickListener {
+            result_num = result_num+(1)
+            tv_result_window.setText(result_num)
+            cnt++
+        })
+
+        btn2.setOnClickListener(View.OnClickListener {
+            result_num = result_num+(2)
+            tv_result_window.setText(result_num)
+            cnt++
+        })
     }
+
+
 }
